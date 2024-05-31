6121584a96a4e3710aa6202578cdb5e3b62a1ab2
Anuj Kumar Sharma <anuj55149@gmail.com>
2024-05-31 19:23:06 +0530

    Chapter 2.4

--- src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -2,7 +2,7 @@ package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.controller
 
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.dto.EmployeeDTO;
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.entities.EmployeeEntity;
-import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.repositories.EmployeeRepository;
+import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.services.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -17,26 +17,27 @@ public class EmployeeController {
 //        return "Secret message: asdfal@#$DASD";
 //    }
 
-    private final EmployeeRepository employeeRepository;
 
-    public EmployeeController(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping(path = "/{employeeId}")
-    public EmployeeEntity getEmployeeById(@PathVariable(name = "employeeId") Long id) {
-        return employeeRepository.findById(id).orElse(null);
+    public EmployeeDTO getEmployeeById(@PathVariable(name = "employeeId") Long id) {
+        return employeeService.getEmployeeById(id);
     }
 
     @GetMapping
-    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false, name = "inputAge") Integer age,
+    public List<EmployeeDTO> getAllEmployees(@RequestParam(required = false, name = "inputAge") Integer age,
                                                 @RequestParam(required = false) String sortBy) {
-        return employeeRepository.findAll();
+        return employeeService.getAllEmployees();
     }
 
     @PostMapping
-    public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity inputEmployee) {
-        return employeeRepository.save(inputEmployee);
+    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployee) {
+        return employeeService.createNewEmployee(inputEmployee);
     }
 
     @PutMapping String updateEmployeeById() {
---
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/services/EmployeeService.java
@@ -0,0 +1,53 @@
+package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.services;
+
+import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.dto.EmployeeDTO;
+import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.entities.EmployeeEntity;
+import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.repositories.EmployeeRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+    private final ModelMapper modelMapper;
+
+    public EmployeeService(EmployeeRepository employeeRepository, ModelMapper modelMapper) {
+        this.employeeRepository = employeeRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+    public EmployeeDTO getEmployeeById(Long id) {
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).orElse(null);
+        return modelMapper.map(employeeEntity, EmployeeDTO.class);
+    }
+
+    public List<EmployeeDTO> getAllEmployees() {
+        List<EmployeeEntity> employeeEntities = employeeRepository.findAll();
+        return employeeEntities
+                .stream()
+                .map(employeeEntity -> modelMapper.map(employeeEntity, EmployeeDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public EmployeeDTO createNewEmployee(EmployeeDTO inputEmployee) {
+//        to check if user is admin
+//        log something
+        EmployeeEntity toSaveEntity = modelMapper.map(inputEmployee, EmployeeEntity.class);
+        EmployeeEntity savedEmployeeEntity = employeeRepository.save(toSaveEntity);
+        return modelMapper.map(savedEmployeeEntity, EmployeeDTO.class);
+    }
+}
+
+
+
+
+
+
+
+
+
