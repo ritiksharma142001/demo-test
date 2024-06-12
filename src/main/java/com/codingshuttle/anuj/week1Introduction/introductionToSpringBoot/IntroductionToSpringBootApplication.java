2e004c406ec9cae829b12ed68cef185474c6ad81
Anuj Kumar Sharma <anuj55149@gmail.com>
2024-06-12 17:50:52 +0530

    1.3 Beans

--- src/main/java/com/codingshuttle/anuj/week1Introduction/introductionToSpringBoot/IntroductionToSpringBootApplication.java
+++ src/main/java/com/codingshuttle/anuj/week1Introduction/introductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -1,13 +1,30 @@
 package com.codingshuttle.anuj.week1Introduction.introductionToSpringBoot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroductionToSpringBootApplication {
+public class IntroductionToSpringBootApplication implements CommandLineRunner {
+
+	@Autowired
+	Apple apple1;
+
+	@Autowired
+	Apple apple2;
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		apple1.eatApple();
+		apple2.eatApple();
+
+		System.out.println(apple1.hashCode());
+		System.out.println(apple2.hashCode());
+	}
 }
