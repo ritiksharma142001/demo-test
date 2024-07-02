46549386bd9c337b3c0e69dbad1993efd1ab7f3c
Anuj Kumar Sharma <anuj55149@gmail.com>
2024-07-02 18:07:00 +0530

    4.1 basic setup

---
+++ src/main/java/com/codingshuttle/anuj/prod_ready_features/prod_ready_features/entities/PostEntity.java
@@ -0,0 +1,20 @@
+package com.codingshuttle.anuj.prod_ready_features.prod_ready_features.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "posts")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+}
