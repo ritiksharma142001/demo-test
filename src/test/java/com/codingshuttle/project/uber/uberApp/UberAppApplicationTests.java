67a7ef090b489fd77419edcb78bb6d177e8ae06a
Anuj Kumar Sharma <anuj55149@gmail.com>
2024-08-31 21:58:45 +0530

    added prod application properties

--- src/test/java/com/codingshuttle/project/uber/uberApp/UberAppApplicationTests.java
+++ src/test/java/com/codingshuttle/project/uber/uberApp/UberAppApplicationTests.java
@@ -8,28 +8,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UberAppApplicationTests {
 
-	@Autowired
-	private EmailSenderService emailSenderService;
-
-	@Test
-	void contextLoads() {
-		emailSenderService.sendEmail(
-				"xijafe6055@apifan.com",
-				"This is the Testing Email",
-				"Body of my email");
-	}
-
-	@Test
-	void sendEmailMultiple() {
-		String emails[] = {
-				"xijafe6055@apifan.com",
-				"contact@codingshuttle.com",
-				"shivanilalwani22@gmail.com",
-				"premmane3092001@gmail.com",
-				"connectankesh@gmail.com"
-		};
-		emailSenderService.sendEmail(emails, "Hello from the UBER Application demo",
-				"This is a good body, Keep coding!");
-	}
-
 }
