96b392966da4ade9caf9444cef9d7a08cdf86872
Anuj Kumar Sharma <anuj55149@gmail.com>
2024-09-20 12:50:31 +0530

    Part - 4, Open Feign added

--- order-service/src/main/java/com/codingshuttle/ecommerce/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/codingshuttle/ecommerce/order_service/controller/OrdersController.java
@@ -1,15 +1,13 @@
 package com.codingshuttle.ecommerce.order_service.controller;
 
+import com.codingshuttle.ecommerce.order_service.clients.InventoryOpenFeignClient;
 import com.codingshuttle.ecommerce.order_service.dto.OrderRequestDto;
 import com.codingshuttle.ecommerce.order_service.service.OrdersService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,12 @@ public class OrdersController {
     @GetMapping("/helloOrders")
     public String helloOrders() {
         return "Hello from Orders Service";
+    }
+
+    @PostMapping("/create-order")
+    public ResponseEntity<OrderRequestDto> createOrder(@RequestBody OrderRequestDto orderRequestDto) {
+        OrderRequestDto orderRequestDto1 = orderService.createOrder(orderRequestDto);
+        return ResponseEntity.ok(orderRequestDto1);
     }
 
     @GetMapping
--- order-service/src/main/java/com/codingshuttle/ecommerce/order_service/service/OrdersService.java
+++ order-service/src/main/java/com/codingshuttle/ecommerce/order_service/service/OrdersService.java
@@ -1,6 +1,9 @@
 package com.codingshuttle.ecommerce.order_service.service;
 
+import com.codingshuttle.ecommerce.order_service.clients.InventoryOpenFeignClient;
 import com.codingshuttle.ecommerce.order_service.dto.OrderRequestDto;
+import com.codingshuttle.ecommerce.order_service.entity.OrderItem;
+import com.codingshuttle.ecommerce.order_service.entity.OrderStatus;
 import com.codingshuttle.ecommerce.order_service.entity.Orders;
 import com.codingshuttle.ecommerce.order_service.repoitory.OrdersRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +20,7 @@ public class OrdersService {
 
     private final OrdersRepository orderRepository;
     private final ModelMapper modelMapper;
+    private final InventoryOpenFeignClient inventoryOpenFeignClient;
 
     public List<OrderRequestDto> getAllOrders() {
         log.info("Fetching all orders");
@@ -29,4 +33,29 @@ public class OrdersService {
         Orders order = orderRepository.findById(id).orElseThrow(() -> new RuntimeException("Order not found"));
         return modelMapper.map(order, OrderRequestDto.class);
     }
+
+    public OrderRequestDto createOrder(OrderRequestDto orderRequestDto) {
+        Double totalPrice = inventoryOpenFeignClient.reduceStocks(orderRequestDto);
+
+        Orders orders = modelMapper.map(orderRequestDto, Orders.class);
+        for(OrderItem orderItem: orders.getItems()) {
+            orderItem.setOrder(orders);
+        }
+        orders.setTotalPrice(totalPrice);
+        orders.setOrderStatus(OrderStatus.CONFIRMED);
+
+        Orders savedOrder = orderRepository.save(orders);
+
+        return modelMapper.map(savedOrder, OrderRequestDto.class);
+    }
 }
+
+
+
+
+
+
+
+
+
+
