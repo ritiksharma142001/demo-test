da6fc5fda9aa078f54d59f8e2bb3db8ff902bea9
Anuj Kumar Sharma <anuj55149@gmail.com>
2024-11-15 17:36:49 +0530

    Part - 2, Services added to gke

--- order-service/src/main/java/com/codingshuttle/ecommerce/order_service/clients/InventoryOpenFeignClient.java
+++ order-service/src/main/java/com/codingshuttle/ecommerce/order_service/clients/InventoryOpenFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "inventory-service", path = "/inventory")
+@FeignClient(name = "inventory-service", path = "/inventory", url = "${INVENTORY_SERVICE_URI:}")
 public interface InventoryOpenFeignClient {
 
     @PutMapping("/products/reduce-stocks")
